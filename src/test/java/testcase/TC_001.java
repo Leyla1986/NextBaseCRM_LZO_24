5b23cd856114ade7ef7c46f23f25e7f50b58f417
Leyla Git <leyla.najafzade.dc@gmail.com>
2021-11-08 11:28:08 -0600

    task 01

---
+++ src/test/java/testcase/TC_001.java
@@ -0,0 +1,31 @@
+package testcase;
+
+public class TC_001 {
+    public static void main(String[] args) {
+
+        /**
+         * Assignment 1 :
+         * Practice website link: http://practice.cybertekschool.com/download
+         * User story:
+         * As a user, I want to download files so that i can save them in my local device. Acceptance Criteria(AC):
+         * 1. "File Downloader" header should be displayed
+         * 2. All the required files should be displayed on the page
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
